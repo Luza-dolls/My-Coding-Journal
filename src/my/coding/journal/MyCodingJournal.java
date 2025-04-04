914d470632257b29a2b3fbf5a0283142d329cdd9
luz_a <luz_a@DESKTOP-3C3LH1P.home>
2025-04-03 21:55:12 -0400

    carga inicial

---
+++ src/my/coding/journal/MyCodingJournal.java
@@ -0,0 +1,9 @@
+
+package my.coding.journal;
+
+public class MyCodingJournal {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo¡ Bienvenido por primera vez a mi vida 03 abril 2025");    }
+    
+}
